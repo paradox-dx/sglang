5e441cb73fc76a4eb63827d58702233ed26d12e3
artetaout <lulala341@gmail.com>
2025-08-11 07:15:28 +0000

    support overlap for all row linear

--- python/sglang/srt/layers/linear.py
+++ python/sglang/srt/layers/linear.py
@@ -1395,6 +1395,9 @@ class RowParallelLinear(LinearBase):
             )
         else:
             self.register_parameter("bias", None)
+        
+        self.gemm_ar_attn_op = None
+        self.gemm_ar_mlp_op = None
 
     def weight_loader(self, param: Parameter, loaded_weight: torch.Tensor):
         input_dim = getattr(param, "input_dim", None)
@@ -1490,6 +1493,15 @@ class RowParallelLinear(LinearBase):
                 param.load_row_parallel_weight(loaded_weight)
 
     def forward(self, input_, skip_all_reduce=False):
+        if get_int_env_var('SGL_USE_TP_OVERLAP', 0) == 1:
+            if self.gemm_ar_attn_op:
+                output = self.gemm_ar_attn_op.forward(input_, self.weight, self.bias)
+            else:
+                output = self.gemm_ar_mlp_op.forward(input_, self.weight, self.bias)
+
+            output_bias = self.bias if self.skip_bias_add else None
+            return output, output_bias
+
         if self.input_is_parallel:
             input_parallel = input_
         else:
--- python/sglang/srt/model_executor/model_runner.py
+++ python/sglang/srt/model_executor/model_runner.py
@@ -441,7 +441,10 @@ class ModelRunner(ModelRunnerKVCacheMixin):
 
         # For weight updates
         self._model_update_group = {}
-        self._weights_send_group = {}
+
+        self.gemm_ar_attn_op = None
+        self.gemm_ar_mlp_op = None
+
 
     def init_mindspore_runner(self):
         # Init the mindspore runner
@@ -641,6 +644,7 @@ class ModelRunner(ModelRunnerKVCacheMixin):
         if self.device == "cuda" or self.device == "musa":
             self.init_cublas()
             self.init_attention_backend()
+            self.init_overlap_gemm_allreduce_operator()
             self.kernel_warmup()
             self.init_device_graphs()
         elif self.device in ["npu", "cpu"]:
@@ -1853,6 +1857,49 @@ class ModelRunner(ModelRunnerKVCacheMixin):
             )
 
         log_info_on_rank0(logger, f"Using KV cache dtype: {self.kv_cache_dtype}")
+
+
+    def init_overlap_gemm_allreduce_operator(self):
+        from sglang.srt.distributed import parallel_state as ps
+        from triton_dist.layers.nvidia import GemmARLayer
+        from triton_dist.utils import (
+            is_nvshmem_multimem_supported, 
+            assert_allclose, 
+            dist_print, 
+            generate_data, 
+            group_profile,
+            initialize_distributed, 
+            nvshmem_barrier_all_on_stream, 
+            perf_func, 
+            finalize_distributed,
+            sleep_async
+        )
+
+        self.gemm_ar_attn_op = GemmARLayer(
+            tp_group=ps._TP_GLOO,
+            max_M=self.model_config.hf_config.max_position_embeddings, 
+            N=self.model_config.hf_config.hidden_size, 
+            K=(self.model_config.hf_config.hidden_size // self.tp_size),
+            input_dtype=torch.bfloat16,
+            output_dtype=torch.bfloat16,
+            local_world_size=self.tp_size, persistent=True, copy_to_local=False,
+            use_ll_kernel=False, NUM_COMM_SMS=2)
+        
+        self.gemm_ar_mlp_op = GemmARLayer(
+            tp_group=ps._TP_GLOO,
+            max_M=self.model_config.hf_config.max_position_embeddings, 
+            N=self.model_config.hf_config.hidden_size, 
+            K=(self.model_config.hf_config.intermediate_size // self.tp_size),
+            input_dtype=torch.bfloat16,
+            output_dtype=torch.bfloat16,
+            local_world_size=self.tp_size, persistent=True, copy_to_local=False,
+            use_ll_kernel=False, NUM_COMM_SMS=2)
+        
+        for each in self.model.model.layers:
+            if each.self_attn.o_proj:
+                each.self_attn.o_proj.gemm_ar_attn_op = self.gemm_ar_attn_op
+            if each.mlp.down_proj:
+                each.mlp.down_proj.gemm_ar_mlp_op = self.gemm_ar_mlp_op
 
     def init_cublas(self):
         """We need to run a small matmul to init cublas. Otherwise, it will raise some errors later."""
